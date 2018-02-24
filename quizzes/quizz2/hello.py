fdbe457a8d55111d1dc41c02b3d64e4b21f3f6af
Juan Chen <juanchen917@gmail.com>
2018-02-23 18:24:19 -0800

    first draft finished

--- quizzes/quizz2/hello.py
+++ quizzes/quizz2/hello.py
@@ -1,27 +1,58 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request, json, Response
 
+app = Flask(__name__)
+users = []
+
+# get homepage
 @app.route("/")
 def hello():
     return "Hello World!"
 
+# create new users
 @app.route("/users",methods=['POST'])
 def new_user():
     name = request.form['name']
-    uid = request.form['id']
-    return 'hello {}!'.format(name) #response should be Json data, 
-    #change the default status code 200 to 201 by the framework
+    uid = 0
+    if len(users) == 0:
+        uid = 1
+        users.append({'name':name, 'id':uid})
+    else:
+        uid = users[len(users)-1]['id'] + 1
+        users.append({'name': name, 'id': uid})
+    newUser = {'id':uid, 'name':name}
+    response = app.response_class(
+        response = json.dumps(newUser),
+        status = 201,
+        mimetype = 'application/json'
+    )
+    return response
 
 
-''''
-1 GET / -> hello world
-2 POST /users
-    'name=foo' 
-    data store {} in memory
-    return 201 & JSON data
+# get users by id
+@app.route("/users/<int:uid>",methods=['GET'])
+def get_user(uid):
+    curUser = {}
+    for user in users:
+        if (user['id'] == uid):
+            curUser = user
+    if len(curUser) != 0:
+        return Response(json.dumps(curUser), status=200, mimetype='application/json')
+    return Response(status=404)
 
-get "/users/1"
-    response: {'id':1, "name": "x"}
+# DELETE users
+@app.route("/users/<int:uid>",methods=['DELETE'])
+def delete_user(uid):
+    i = 0
+    flag = False
+    for i in range(len(users)):
+        if (users[i]['id'] == uid):
+            flag = True
+            break
+    if flag:
+        users.pop(i)
+        return Response(status=204)
+    return Response(status=400)
 
-3. GET users/1 ->200
-4. DELETE users/1 =>204
+
+
